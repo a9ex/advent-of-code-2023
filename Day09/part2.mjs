b3829f9142db36b1474114e0ebbf08490adf866b
A1ex <a1ex@a1ex.fr>
2023-12-09 18:08:37 +0100

    feat(Day09): **

---
+++ Day09/part2.mjs
@@ -0,0 +1,22 @@
+import fs from 'node:fs'
+
+const input = fs.readFileSync('./input').toString()
+const histories = input.split('\n')
+let extrapolatedValues = 0
+
+for (const history of histories) {
+    const sequences = [history.split(' ').map(Number)]
+    while (!sequences.at(-1).every(el => el === 0)) {
+        const newSequence = []
+        for (let i = 0; i < sequences.at(-1).length - 1; i++) {
+            newSequence.push(sequences.at(-1)[i + 1] - sequences.at(-1)[i])
+        }
+        sequences.push(newSequence)
+    }
+    for (let i = 0; i < sequences.length - 1; i++) {
+        sequences.at(-2 - i).unshift(sequences.at(-2 - i).at(0) - sequences.at(-1 - i).at(0))
+    }
+    extrapolatedValues += sequences[0].at(0)
+}
+
+console.log(extrapolatedValues)
